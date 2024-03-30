a2136e24cc848821da9b8327c5efc04b82886a81
Allef Soares <allefts@localhost-live.lan>
2024-03-30 00:22:02 -0500

    setting up some auth/auth routing

--- Server/cmd/main.go
+++ Server/cmd/main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"text/template"
 
 	"github.com/allefts/muveez_server/internals/db"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
 func main() {
 	e := echo.New()
