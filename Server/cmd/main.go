95bd78814e174cd789196ca37c72122780fddb9e
Allef Soares <alleftsoares@gmail.com>
2024-05-14 13:26:18 -0500

    restarting backend, want to add oauth

--- Server/cmd/main.go
+++ Server/cmd/main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
+func main(){
 
-	"github.com/allefts/muveez_server/internals/db"
-	"github.com/allefts/muveez_server/internals/handlers"
-	"github.com/allefts/muveez_server/internals/models"
-	"github.com/allefts/muveez_server/internals/routes"
-	"github.com/allefts/muveez_server/internals/services"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-)
-
-func main() {
-	e := echo.New()
-
-	// e.Use(middleware.Logger())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	}))
-
-	//Initializing DB
-	db, err := db.ConnectDB("../internals/db/Muveez.db")
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-	}
-
-	userService := services.NewUserService(models.User{}, db)
-	authHandler := handlers.NewAuthHandler(userService)
-
-	routes.SetupRoutes(e, authHandler)
-	e.Logger.Fatal(e.Start(":8000"))
 }
--- Server/internals/models/SignInCredentials.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type SignInCrendentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-}
